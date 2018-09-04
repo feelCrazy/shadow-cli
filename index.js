6611a5d5739c74e5010ce6feac41cbf8ba362b3e
wming <btming217@gmail.com>
2018-09-04 17:01:38 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+'use strict';
+
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const figlet = require("figlet");
+const shell = require("shelljs");
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+
+const init = () => {
+    console.log(
+        chalk.green(
+            figlet.textSync("Shadow CLI", {
+                font: "Ghost",
+                horizontalLayout: "default",
+                verticalLayout: "default",
+            })
+        )
+    );
+};
+
+const askQuestions = () => {
+    const questions = [{
+            name: "FILENAME",
+            type: "input",
+            message: "What is the name of the file without extension?"
+        },
+        {
+            type: "list",
+            name: "EXTENSION",
+            message: "What is the file extension?",
+            choices: [".rb", ".js", ".php", ".css"],
+            filter: function (val) {
+                return val.split(".")[1];
+            }
+        }
+    ];
+    return inquirer.prompt(questions);
+};
+
+const createFile = (filename, extension) => {
+    const filePath = `${process.cwd()}/${filename}.${extension}`
+    shell.touch(filePath);
+    return filePath;
+};
+
+const success = filepath => {
+    console.log(
+        chalk.white.bgGreen.bold(`Done! File created at ${filepath}`)
+    );
+};
+
+const run = async () => {
+    // show script introduction
+    init();
+
+    // ask questions
+    const answers = await askQuestions();
+    const {
+        FILENAME,
+        EXTENSION
+    } = answers;
+
+    // create the file
+    const filePath = createFile(FILENAME, EXTENSION);
+
+    // show success message
+    success(filePath);
+};
+
+if (major < 4) {
+    console.error(
+        chalk.red(
+            'You are running Node ' +
+            currentNodeVersion +
+            '.\n' +
+            'Create React App requires Node 4 or higher. \n' +
+            'Please update your version of Node.'
+        )
+    );
+    process.exit(1);
+}
+run();
